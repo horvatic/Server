3a7843b0a9a90b7cbfcd39b1a5f60bae0e3d5631
Shawn Horvatic <SH50169@pekininsurance.com>
2016-05-25 14:55:30 -0500

    Intergration test added

--- Server/Server.Test/LiveTest.cs
+++ Server/Server.Test/LiveTest.cs
@@ -8,6 +8,28 @@ namespace Server.Test
     public class LiveTest
     {
         [Fact]
+        public void Live_User_Presses_CTRL_C_Finsh_Request_No_New_Request()
+        {
+            var endPoint = new IPEndPoint((IPAddress.Loopback), 55999);
+            var manager = new ZSocket(endPoint);
+            var testingServer = new MainServer(manager, new WebPageMaker(55999),
+                null, new DirectoryProcessor(),
+                new FileProcessor());
+            var testServerThread = new Thread(() => RunServerUntilEndRequest(testingServer));
+            testServerThread.Start();
+            for (var i = 0; i < 100; i++)
+            {
+                new Thread(() => WebRequest.Create(@"http://localhost:55999//ShawnDocs/Apprenticeships/week6/LargePDFs/Greater10/h2450081.pdf").GetResponse()).Start();
+            }
+            
+            testingServer.StopNewConn();
+            testingServer.CleanUp();
+
+            var wrFailurl = WebRequest.Create(@"http://localhost:55999//ShawnDocs/Apprenticeships/week6/LargePDFs/Greater10/h2450081.pdf");
+            Assert.Throws<WebException>(() => (wrFailurl.GetResponse()));
+        }
+
+        [Fact]
         public void Make_Web_Request()
         {
             var endPoint = new IPEndPoint((IPAddress.Loopback), 4321);
@@ -43,22 +65,20 @@ namespace Server.Test
         [Fact]
         public void Make_Web_Request_For_File_Not_Accpeting_New_Connections_Hello_World()
         {
-            var endPoint = new IPEndPoint((IPAddress.Loopback), 65418);
-            //var manager = new DataManager(new SocketProxy(), endPoint);
+            var endPoint = new IPEndPoint((IPAddress.Loopback), 45418);
             var manager = new ZSocket(endPoint);
-            var testingServer = new MainServer(manager, new WebPageMaker(65418), 
+            var testingServer = new MainServer(manager, new WebPageMaker(45418),
                 null, new DirectoryProcessor(),
                 new FileProcessor());
             var testServerThread = new Thread(() => RunServerUntilEndRequest(testingServer));
             testServerThread.Start();
-            var wrGeturl = WebRequest.Create(@"http://localhost:65418/");
+            var wrGeturl = WebRequest.Create(@"http://localhost:45418/");
             wrGeturl.GetResponse();
             testingServer.StopNewConn();
             testingServer.CleanUp();
-            var wrFailurl = WebRequest.Create(@"http://localhost:65418/");
+            var wrFailurl = WebRequest.Create(@"http://localhost:45418/");
             Assert.Throws<WebException>(() => (wrFailurl.GetResponse()));
         }
-
 
         public void RunServerUntilEndRequest(IMainServer server)
         {
@@ -66,8 +86,8 @@ namespace Server.Test
             {
                 server.Run();
             }
-            
         }
+
         public void RunServerNoUntilEndRequest(IMainServer server)
         {
             server.Run();
